e7a7bc680c4b56659883ac804a912a8a6b1c7349
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-02-13 16:05:42 +0100

    modules

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -6,7 +6,7 @@
 // use ???;
 
 fn main() {
-    match SystemTime::now().duration_since(UNIX_EPOCH) {
+    match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
         Ok(n) => println!("1970-01-01 00:00:00 UTC was {} seconds ago!", n.as_secs()),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
